4b6e756a1c71743a92b3286f5f58aaef56217372
qmaasn <mason.smith07@outlook.com>
2026-09-13 15:12:11 +0100

    Remove unused command aliases
    Removed several command aliases for various commands to streamline the command mapping.

--- src/config/commands/commandAliases.js
+++ src/config/commands/commandAliases.js
@@ -4,21 +4,6 @@
  */
 
 export const commandAliases = {
-    'bal': 'balance',
-    'money': 'balance',
-    'cash': 'balance',
-
-    'dep': 'deposit',
-    'with': 'withdraw',
-    'work': 'work',
-    'daily': 'daily',
-    'gamble': 'gamble',
-    'bet': 'gamble',
-    'rob': 'rob',
-    'crime': 'crime',
-    'pay': 'pay',
-    'give': 'pay',
-    'send': 'pay',
 
     'ping': 'ping',
     'help': 'help',
@@ -34,41 +19,10 @@ export const commandAliases = {
     'untimeout': 'untimeout',
     'unmute': 'untimeout',
 
-    'rank': 'rank',
-    'lvl': 'rank',
-    'xp': 'rank',
-    'leaderboard': 'leaderboard',
-    'lb': 'leaderboard',
-    'top': 'leaderboard',
-
-    'shop': 'shop',
-    'buy': 'buy',
-    'inventory': 'inventory',
-    'inv': 'inventory',
-    'items': 'inventory',
-
     'user': 'userinfo',
     'avatar': 'avatar',
     'pfp': 'avatar',
     'icon': 'avatar',
-
-    'bd': 'birthday',
-    'bday': 'birthday',
-    'b': 'birthday',
-
-    'flip': 'flip',
-    'coin': 'flip',
-    'roll': 'roll',
-    'dice': 'roll',
-    'fight': 'fight',
-
-    'gcreate': 'gcreate',
-    'gstart': 'gcreate',
-    'gend': 'gend',
-    'gstop': 'gend',
-    'gdelete': 'gdelete',
-    'greroll': 'greroll',
-    'groll': 'greroll',
 
     'ticket': 'ticket',
     't': 'ticket',
